9f1dbab7d485ef134af6d58387364374f4eff75e
Theemiss <midinfotn401@gmail.com>
2022-02-14 11:26:33 +0100

    update

--- Permission/task_1.bash
+++ Permission/task_1.bash
@@ -1,7 +1,6 @@
 #! /bin/bash
-touch hello_again file1 file2 file3
-chmod a+x file1
-chmod 700 file2
-chmod 753 file3
-chmod --reference=hello hello_again
-mkdir -m 751 dir1
+whoami
+groups
+touch hello world
+chmod u+x hello
+chmod ug+x,o+r world
---
+++ Permission/task_3.bash
@@ -0,0 +1,3 @@
+#! /bin/bash
+sudo useradd tekouin
+su tekouin
